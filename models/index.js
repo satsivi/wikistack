8119eb0fd8284848e6f45075b10a9d8dc503be8e
Gabby Josebachvili <gabbyjose@gmail.com>
2018-01-22 13:05:49 -0500

    working on adding a hook

--- models/index.js
+++ models/index.js
@@ -34,6 +34,18 @@ const Page = db.define('page', {
   }
 });
 
+Page.hook('beforeValidate', function(title) {
+  console.log('hello world');
+  if (!this.title){
+    this.urlTitle = Math.random().toString(36).substring(2, 12);
+  }
+  else {
+    this.urlTitle = title.replace(/\s+/g, '_').replace(/\W/g, '');
+  }
+  console.log(this.urlTitle);
+  //the equations are working and the function is running evey time we post BUT we cannot 'attach' the url title we generate to the instance of the page
+});
+
 const User = db.define('user', {
   name: {
     type: Sequelize.STRING,
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models');
+const Page = models.Page;
+const User = models.User;
 
 // router.get('/', function(req, res, next){
 //   res.send('You\'ve reached the wiki page!!');
 // });
 
 router.get('/', function(req, res, next) {
-  res.send('got to GET /wiki/');
+  res.redirect('/');
 });
 
 router.post('/', function(req, res, next) {
-  res.send('got to POST /wiki/');
+  var page = Page.build({
+    title: req.body.title,
+    content: req.body.content
+  });
+  //console.log('this is my page: ', page);
+  page.save()
+  .then(res.json(page));
+
+  //console.log(page);
+  //.catch(console.log(err));
+
 });
 
 router.get('/add', function(req, res, next) {
-  res.send('got to GET /wiki/add');
+  res.render('addpage');
 });
 
 
